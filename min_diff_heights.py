1ba0050f12256182df2f020570ca3f9861cc1af1
Noah <noahjoh11@gmail.com>
2023-09-05 22:34:02 +0530

    Problem solved successfully

---
+++ min_diff_heights.py
@@ -0,0 +1,28 @@
+class Solution:
+    def getMinDiff(self, arr, n, k):
+        if n <= 1:
+            return 0
+
+        arr.sort()
+
+        diff = max(arr) - min(arr)
+
+        for i in range(1, n):
+            min_val = min(arr[i]-k, arr[0]+k)
+            max_val = max(arr[i-1]+k, arr[-1]-k)
+            diff = min(max_val - min_val, diff)
+
+        return diff
+
+
+# Driver Code
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        k = int(input())
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.getMinDiff(arr, n, k)
+        print(ans)
+        tc -= 1
