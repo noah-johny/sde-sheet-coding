21004bd17f69d18d26cf1180d66cd46184a67008
Noah <noahjoh11@gmail.com>
2023-09-06 14:35:50 +0530

    Problem solved successfully

---
+++ stock_buy_sell.py
@@ -0,0 +1,38 @@
+class Solution:
+    def stockBuySell(self, A, n):
+        stock = []
+
+        for i in range(n - 1):
+            if A[i + 1] > A[i]:
+                stock.append((i, i + 1))
+
+        return stock
+
+
+# Driver Code
+def check(ans, A, p):
+    c = 0
+    for i in range(len(ans)):
+        c += A[ans[i][1]]-A[ans[i][0]]
+    if (c == p):
+        return 1
+    else:
+        return 0
+
+
+if __name__ == '__main__':
+    t = int(input())
+    while (t > 0):
+        n = int(input())
+        A = [int(x) for x in input().strip().split()]
+        ob = Solution()
+        ans = ob.stockBuySell(A, n)
+        p = 0
+        for i in range(n-1):
+            p += max(0, A[i+1]-A[i])
+        if (len(ans) == 0):
+            print("No Profit", end="")
+        else:
+            print(check(ans, A, p), end="")
+        print()
+        t -= 1
