c0ae4e470b7e677d0d6c7831b05a89aa8799f97e
Noah <noahjoh11@gmail.com>
2023-09-13 16:29:16 +0530

    Problem solved successfully

---
+++ unique_rows_in_matrix.py
@@ -0,0 +1,42 @@
+from typing import List
+
+
+class Solution:
+    def uniqueRow(self, row: int, col: int, M: List[List[int]]) -> List[List[int]]:
+        result = []
+
+        for row in M:
+            row_tuple = tuple(row)
+
+            if row_tuple not in result:
+                result.append(row_tuple)
+
+        return result
+
+
+# Driver Code
+def main():
+    testcase = int(input())
+    while (testcase):
+        s = input().split()
+        row = int(s[0])
+        col = int(s[1])
+        matrix = [[None for _ in range(col)]for _ in range(row)]
+        s = input().split()
+        for i in range(row):
+            for j in range(col):
+                matrix[i][j] = int(s[i*col+j])
+
+        ob = Solution()
+        a = ob.uniqueRow(row, col, matrix)
+
+        for row in a:
+            for value in row:
+                print(value, end=" ")
+            print("$", end="")
+        print()
+        testcase -= 1
+
+
+if __name__ == "__main__":
+    main()
